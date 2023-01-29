76ee8dc5abb42cf504ae32b52dfbe395514721df
Edgar Martinez <edgar.agustin.martinez@gmail.com>
2023-01-28 22:58:54 -0600

    Add routing with auth pages design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import {Link, Outlet, Route, Routes} from "react-router-dom";
+import {Button, ConfigProvider} from "antd";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    return (
+        <ConfigProvider
+            theme={{
+                token: {
+                    colorPrimary: '#00b96b',
+                },
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <div className="App">
+                <Routes>
+                    <Route path={"/"} element={<Layout/>}>
+                        <Route index element={<Home/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                        <Route path="/signup" element={<Signup/>}/>
+                    </Route>
+                </Routes>
+            </div>
+        </ConfigProvider>
+    );
+}
+
+function Layout() {
+    return (
+        <div>
+            <nav className={"navbar"}>
+                <Link to="/" className={"App-link"}>Developer Match</Link>
+                <ul>
+                    <li>
+                        <Button type="primary">
+                            <Link to="/login">Log In</Link>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button type="default">
+                            <Link to="/signup">Sign Up</Link>
+                        </Button>
+                    </li>
+                </ul>
+            </nav>
+
+            <hr/>
+
+            {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+            <main>
+                <Outlet/>
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/signup.js
@@ -0,0 +1,53 @@
+import { Button, Form, Input } from 'antd';
+
+const onFinish = (values) => {
+    console.log('Success:', values);
+};
+
+const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+};
+
+export default function Signup() {
+    return (
+        <Form
+            name="basic"
+            layout="vertical"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+
+            <Form.Item
+                label="Confirm your password"
+                name="password_confirm"
+                rules={[{ required: true, message: 'Please input your password confirmation!' }]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item className={"last-input"} wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+}
